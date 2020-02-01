fc89af590d9bf4d7b571d5d620c58e3280ad7249
Jaderson Barreto Sathler <jbsathler@gmail.com>
2020-02-01 18:12:33 -0300

    [DB] In-memory Database
    - Implementa banco de dados em memória;
    - Adiciona recursos do teste (CSV com os dados e PDF com instruções de implementação).

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
+import { DB         } from './server/db';
 import { Server     } from './server/server';
 import { mainRouter } from './main.router';
 
+const db     = new DB();
 const server = new Server();
 
-server.bootstrap([
-    mainRouter
-]).then(server => {
-    console.log('Server is listening on:', server.application.address());
-}).catch(error => {
-    console.log('Server failed to start...');
-    console.error(error);
-    process.exit(1);
+db.bootstrap().then(() => {
+    server.bootstrap([
+        mainRouter
+    ]).then(server => {
+        console.log('Server is listening on:', server.application.address());
+    }).catch(error => {
+        console.log('Server failed to start...');
+        console.error(error);
+        process.exit(1);
+    });
 });
